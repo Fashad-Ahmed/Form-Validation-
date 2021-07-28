d3557edb8ac4dcc31799b36f864473b7d750d7c0
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2021-07-29 03:04:30 +0500

    UI and Link created

---
+++ src/Components/Login.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Grid, Paper, Avatar, TextField, Button, Typography, Link } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const LogIn = ({ handleChange }) => {
+
+    const paperStyle = { padding: 20, height: '70vh', width: 280, margin: "20px auto" }
+    const avatarStyle = { backgroundColor: 'turquoise' }
+    const btnstyle = { margin: '8px 0' }
+    const initialValues = {
+        username: '',
+        password: '',
+        remember: false
+    }
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+                    <h2>Log In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required />
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            name="checkedB"
+                            color="primary"
+                        />
+                    }
+                    label="Remember me"
+                />
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+
+
+                <Typography >
+                    <Link href="#" >
+                        Forgot password ?
+                    </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                    <Link href="#" onClick={() => handleChange('event', 1)} >
+                        Sign Up
+                    </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+    )
+};
+
+export default LogIn;
---
+++ src/Container/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import LogIn from '../Components/Login';
+import SignUp from '../Components/Signup';
+
+const Container = () => {
+    const [value, setValue] = useState(0);
+    const paperStyle = { width: 340, margin: "20px auto" }
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    }
+
+    function TabPanel(props) {
+        const { children, value, index, ...other } = props;
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
+        );
+    }
+
+    return (
+        <Paper elevation={20} style={paperStyle}>
+            <Tabs
+                value={value}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={handleChange}
+                aria-label="disabled tabs example"
+            >
+                <Tab label="Sign In" />
+
+                <Tab label="Sign Up" />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                <LogIn handleChange={handleChange} />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <SignUp />
+            </TabPanel>
+        </Paper>
+
+    )
+
+}
+
+export default Container;
